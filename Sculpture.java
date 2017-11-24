6b1248acda72a051c9e546f2c161aefff1d9e62f
Timrwwatson <33657006+Timrwwatson@users.noreply.github.com>
2017-11-24 18:27:37 +0000

    Revert "Added To String"
    This reverts commit 40442fb2e473cd6ceccec5c4de3801d3c3baf215.

--- Sculpture.java
+++ Sculpture.java
@@ -96,14 +96,4 @@ public class Sculpture extends Artwork {
     public void setMaterial(String newMaterial) {
         this.material = newMaterial;
     }
-
-    @Override
-    public String toString() {
-        return "Sculpture{" +
-                "width=" + width +
-                ", height=" + height +
-                ", depth=" + depth +
-                ", material='" + material + '\'' +
-                "} " + super.toString();
-    }
 }
