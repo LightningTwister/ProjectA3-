2fb10b150a2daa3cd7246125f6c62387c6326952
Timrwwatson <33657006+Timrwwatson@users.noreply.github.com>
2017-12-09 21:12:36 +0000

    SIT NERD

--- FileWriter.java
+++ FileWriter.java
@@ -1,119 +1,116 @@
-import javax.xml.crypto.Data;
-import java.awt.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
+
+/**
+ * Class to save system data to file
+ *
+ * @author Nick
+ * @version 1
+ */
 public class FileWriter {
     private static String delim = ",";
 
-    private static void writeDataToFile(PrintWriter out, String type){
+    /**
+     * Method that takes a writer and the type of file to be written
+     *
+     * @param out  Writer to the file that will be used
+     * @param type type of objects to be writen to file
+     */
+    private static void writeDataToFile(PrintWriter out, String type) {
 
-         if (type.toLowerCase().equals("userlist")){
-             //System.out.println("Writing users to file.");
-             for (UserProfiles e : Run.database.getAllUsers()){
-                 out.print(e.getId()+ delim);
-                 out.print(e.getUserName() + delim);
-                 out.print(e.getFirstName() + delim);
-                 out.print(e.getLastName() + delim);
-                 out.print(e.getPhoneNumber()+ delim);
-                 for (String s : e.getAddress()){
-                     out.print(s + delim);
-                 }
-                 out.print(e.getPostCode() + delim);
+        if (type.toLowerCase().equals("userlist")) {
+            //System.out.println("Writing users to file.");
+            for (UserProfiles e : Run.database.getAllUsers()) {
+                out.print(e.getId() + delim);
+                out.print(e.getUserName() + delim);
+                out.print(e.getFirstName() + delim);
+                out.print(e.getLastName() + delim);
+                out.print(e.getPhoneNumber() + delim);
+                for (String s : e.getAddress()) {
+                    out.print(s + delim);
+                }
+                out.print(e.getPostCode() + delim);
 
-                 out.print( e.getLogInDate()+ delim);
-                 out.print(e.getProfilePicture()+delim);
-                 for(Integer a: e.getFaveUsers()){
-                     out.print(a + delim);
-                 }
-                 out.println();
-             }
+                out.print(e.getLogInDate() + delim);
+                out.print(e.getProfilePicture() + delim);
+                for (Integer a : e.getFaveUsers()) {
+                    out.print(a + delim);
+                }
+                out.println();
+            }
 
-             out.close();
-         }else if (type.toLowerCase().equals("artworklist")) {
-             for (Artwork a : Run.database.getAllArtworks()) {
-                 if (a instanceof Painting) {
+            out.close();
+        } else if (type.toLowerCase().equals("artworklist")) {
+            for (Artwork a : Run.database.getAllArtworks()) {
+                if (a instanceof Painting) {
 
-                     Painting p = (Painting)a;
-                     //System.out.println("Writing painting to file.");
-                         out.print("p" + delim);
-                         out.print(p.getArtworkTitle() + delim);
-                         out.print(p.getArtworkDescription() + delim);
+                    Painting p = (Painting) a;
+                    //System.out.println("Writing painting to file.");
+                    out.print("p" + delim);
+                    out.print(p.getArtworkTitle() + delim);
+                    out.print(p.getArtworkDescription() + delim);
 
-                         out.print(p.getArtworkCreator() + delim);
-                         out.print(p.getArtworkYearCreated() + delim);
-                         out.print(p.getReservePrice() + delim);
-                         out.print(p.getNumOfBids() + delim);
-                         out.print(p.getArtworkSeller() + delim);
-                         out.print(231240124+delim);
-                         out.print(p.getWidth() + delim);
-                         out.print(p.getHeight() + delim);
-                         out.print(p.getId()+ delim);
-                         for(String nextPicture: p.getPictures()){
-                             out.print(nextPicture+delim);
-                         }
+                    out.print(p.getArtworkCreator() + delim);
+                    out.print(p.getArtworkYearCreated() + delim);
+                    out.print(p.getReservePrice() + delim);
+                    out.print(p.getNumOfBids() + delim);
+                    out.print(p.getArtworkSeller() + delim);
+                    out.print(231240124 + delim);
+                    out.print(p.getWidth() + delim);
+                    out.print(p.getHeight() + delim);
+                    out.print(p.getId() + delim);
+                    for (String nextPicture : p.getPictures()) {
+                        out.print(nextPicture + delim);
+                    }
 
-                         out.println();
+                    out.println();
 
-                 }
+                } else if (a instanceof Sculpture) {
 
-                     else if (a instanceof Sculpture) {
-
-                        Sculpture s = (Sculpture)a;
-                        //System.out.println("Writing sculpture to file.");
-                        out.print("s" + delim);
-                         out.print(s.getArtworkTitle() + delim);
-                         out.print(s.getArtworkDescription() + delim);
-                         out.print(s.getArtworkCreator() + delim);
-                         out.print(s.getArtworkYearCreated() + delim);
-                         out.print(s.getReservePrice() + delim);
-                         out.print(s.getNumOfBids() + delim);
-                         out.print(s.getArtworkSeller() + delim);
-                         out.print(231240124+delim);
-                         out.print(s.getWidth() + delim);
-                         out.print(s.getHeight() + delim);
-                         out.print(s.getId()+delim);
-                         out.print(s.getDepth()+ delim);
-                         out.print(s.getMaterial()+ delim);
-                     for(String nextPicture: s.getPictures()){
-                         out.print(nextPicture+delim);
-                     }
-                         out.println();
-                 }
+                    Sculpture s = (Sculpture) a;
+                    //System.out.println("Writing sculpture to file.");
+                    out.print("s" + delim);
+                    out.print(s.getArtworkTitle() + delim);
+                    out.print(s.getArtworkDescription() + delim);
+                    out.print(s.getArtworkCreator() + delim);
+                    out.print(s.getArtworkYearCreated() + delim);
+                    out.print(s.getReservePrice() + delim);
+                    out.print(s.getNumOfBids() + delim);
+                    out.print(s.getArtworkSeller() + delim);
+                    out.print(231240124 + delim);
+                    out.print(s.getWidth() + delim);
+                    out.print(s.getHeight() + delim);
+                    out.print(s.getId() + delim);
+                    out.print(s.getDepth() + delim);
+                    out.print(s.getMaterial() + delim);
+                    for (String nextPicture : s.getPictures()) {
+                        out.print(nextPicture + delim);
+                    }
+                    out.println();
+                }
 
 
-             }
-             out.close();
+            }
+            out.close();
 
 
-         }/*else if (type.toLowerCase().equals("bidhistorylist")){
-             System.out.println("Writing bidhistory to file.");
-             for (Artwork a : Run.database.getAllArtworks()){
-                 out.print(a.getId());
-                 for(int i = 0; i < a.getArtworkBid().getBidHistory().getAmountBidArray().size(); i++){
-                     out.print(delim + a.getArtworkBid().getBidHistory().getProfilesOfBidders().get(i));
-                     out.print(delim + a.getArtworkBid().getBidHistory().getAmountBidArray().get(i));
-                     out.print(delim + (a.getArtworkBid().getBidHistory().getDateArray().get(i)));
-                     out.println();
-                 }
-             }
-             out.close();
-         }*/
-
+        }
     }
 
-    public static void openFile(String fileName, String type){
+    /**
+     *  Method to open the file to be written too
+     * @param fileName Path to the file to be written too
+     * @param type Type of objects that will be written to file
+     */
+    public static void openFile(String fileName, String type) {
         File outputFile = new File(fileName);
         PrintWriter out = null;
         try {
             out = new PrintWriter(outputFile);
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("Cannot open " + fileName);
             System.exit(0);
         }
