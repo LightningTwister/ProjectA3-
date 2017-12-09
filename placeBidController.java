03266b0860945f8e0ab93594aa5c8959f18366ba
Timrwwatson <33657006+Timrwwatson@users.noreply.github.com>
2017-12-09 21:34:50 +0000

    #SIT

--- placeBidController.java
+++ placeBidController.java
@@ -2,7 +2,6 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -13,24 +12,24 @@ import java.nio.file.FileAlreadyExistsException;
  * Created by LT on 09/12/2017.
  */
 public class placeBidController {
-    Artwork artwork;
+    private Artwork artwork;
 
     @FXML
-    Pane rootPane;
+    private Pane rootPane;
+    @FXML
+    private TextField amountField;
+    @FXML
+    private VBox vBox;
+    @FXML
+    private Label errorTooLow, errorBidTwice;
+    @FXML
+    private Button confirm, cancel;
 
     @FXML
-    TextField amountField;
 
-    @FXML
-    VBox vBox;
-
-    @FXML
-    Label errorTooLow, errorBidTwice;
-
-    @FXML
-    Button confirm, cancel;
-
-    @FXML
+    /**
+     * Initilise method that loads the gui elements to methods
+     */
     public void initialize() {
         errorTooLow.setBackground(new Background(new BackgroundFill(
                 Color.web("#FFCCCC"), CornerRadii.EMPTY, Insets.EMPTY)));
@@ -40,43 +39,51 @@ public class placeBidController {
         errorTooLow.setVisible(false);
         errorBidTwice.setVisible(false);
 
-        cancel.setOnAction(e ->{
+        cancel.setOnAction(e -> {
             goBack();
         });
 
-        confirm.setOnAction(e ->{
+        confirm.setOnAction(e -> {
             try {
                 errorTooLow.setVisible(false);
                 errorBidTwice.setVisible(false);
                 placeBid();
                 goBack();
-            }catch (FileAlreadyExistsException c1){
+            } catch (FileAlreadyExistsException c1) {
                 errorBidTwice.setVisible(true);
 
-            }catch (Exception e2){
+            } catch (Exception e2) {
                 errorTooLow.setVisible(true);
             }
 
         });
 
-
-
-
     }
 
-    public void setArtwork(Artwork artwork){
+    /**
+     * Gets the artwork to be bid on and saves the reference
+     * @param artwork Artwork object a bid is being added for
+     */
+    public void setArtwork(Artwork artwork) {
         this.artwork = artwork;
     }
 
-    private void placeBid() throws Exception{
-        if(Run.database.getCurrentUser().getId() == artwork.getHighestBid().getUserID()){
+    /**
+     *  Method that checks the bid being created is valid
+     * @throws Exception
+     */
+    private void placeBid() throws Exception {
+        if (Run.database.getCurrentUser().getId() == artwork.getHighestBid().getUserID()) {
             throw new FileAlreadyExistsException("Can't Bid when you're the highest bidder");
         }
         Run.database.placeBid(Double.valueOf(amountField.getText()), artwork.getId());
     }
 
+    /**
+     * Exit window
+     */
     @FXML
-    private void goBack(){
+    private void goBack() {
         Utilities.closeWindow(rootPane);
     }
 }
